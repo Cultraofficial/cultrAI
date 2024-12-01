fb04063d541ac5f18259716798210d2bd333517a
Cultraofficial <tropicalstormllc@gmail.com>
2024-12-01 09:12:17 -0500

    Add organized_ai_models/misc/Untitled11_snippet_20.py

---
+++ organized_ai_models/misc/Untitled11_snippet_20.py
@@ -0,0 +1,11 @@
+# Change directory to the cloned repository
+%cd /content/sacrifice-in-Cultra
+
+# Add changes
+!git add .
+
+# Commit changes
+!git commit -m "Add workflow and training script"
+
+# Push changes
+!git push https://{personal_access_token}@github.com/Cultraofficial/sacrifice-in-Cultra.git main
