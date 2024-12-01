ac9ee3071d5cd26ce710c3a0548be9a5d2f86c50
Cultraofficial <tropicalstormllc@gmail.com>
2024-12-01 09:12:05 -0500

    Add organized_ai_models/misc/Untitled11_snippet_11.py

---
+++ organized_ai_models/misc/Untitled11_snippet_11.py
@@ -0,0 +1,7 @@
+# Save remaining models to Google Drive
+save_model_to_drive("distilbert_model", "./distilbert_model")
+save_model_to_drive("gpt_neo_model", "./gpt_neo_model")
+save_model_to_drive("t5_large_model", "./t5_large_model")
+save_model_to_drive("stable_diffusion_model", "./stable_diffusion_model")
+save_model_to_drive("whisper_tiny_model", "./whisper_tiny_model")
+# Add more as needed based on the remaining models in memory
