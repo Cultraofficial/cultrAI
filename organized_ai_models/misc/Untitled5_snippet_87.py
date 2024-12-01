43d2176ce9a1d91cb88aa1b2a8e6053c9a3d9f08
Cultraofficial <tropicalstormllc@gmail.com>
2024-12-01 09:05:40 -0500

    Add organized_ai_models/misc/Untitled5_snippet_87.py

---
+++ organized_ai_models/misc/Untitled5_snippet_87.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+from pyngrok import ngrok
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Firebase setup
+if not firebase_admin._apps:
+    cred = credentials.Certificate('/content/drive/My Drive/BrandNewKey.json')
+    firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+# Start Ngrok tunnel
+public_url = ngrok.connect(5000)
+print("Ngrok Tunnel URL:", public_url)
+
+# Sample route to add a user
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+
+    user_data = {
+        "username": username,
+        "email": email,
+        "interaction_count": 0
+    }
+    db.collection('users').document(username).set(user_data)
+    return jsonify({"status": "success", "data": user_data})
+
+if __name__ == '__main__':
+    app.run(port=5000)
