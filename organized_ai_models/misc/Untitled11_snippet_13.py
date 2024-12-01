113bc471ac810bc415691063c2c9e3ff44156d49
Cultraofficial <tropicalstormllc@gmail.com>
2024-12-01 09:12:08 -0500

    Add organized_ai_models/misc/Untitled11_snippet_13.py

---
+++ organized_ai_models/misc/Untitled11_snippet_13.py
@@ -0,0 +1,5 @@
+import shutil
+
+# Move the GPT-Neo 1.3B model to Google Drive
+shutil.move("./gpt_neo_1.3B_model", "/content/drive/My Drive/ModelStorage/gpt_neo_1.3B_model")
+print("GPT-Neo 1.3B model saved to Google Drive.")
