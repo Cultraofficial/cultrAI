60a6cd91e225cdabf8ea588b4325f5c9289ddb71
Cultraofficial <tropicalstormllc@gmail.com>
2024-12-01 09:17:34 -0500

    Add organized_ai_models/misc/Untitled19_snippet_81.py

---
+++ organized_ai_models/misc/Untitled19_snippet_81.py
@@ -0,0 +1,7 @@
+{
+    "type": "service_account",
+    "project_id": "gen-lang-client-0492208227",
+    "private_key_id": "YOUR_KEY_ID",
+    "client_email": "your-service-account-email@your-project-id.iam.gserviceaccount.com",
+    ...
+}
