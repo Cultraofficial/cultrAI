c58302f7a5d67b45c25fd2ae657542bf2f7cf55c
Cultraofficial <tropicalstormllc@gmail.com>
2024-12-01 09:10:53 -0500

    Add organized_ai_models/misc/Untitled5_snippet_265.py

---
+++ organized_ai_models/misc/Untitled5_snippet_265.py
@@ -0,0 +1,7 @@
+from huggingface_hub import login
+
+# Hugging Face Authentication
+login("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")  # Your latest Hugging Face token
+
+# Ngrok Authentication
+!ngrok authtoken 2ooma1TJYZL1Mh97aTguEPFVWB1_2B5rAwbuoEGMa1QsqRMRa  # Your Ngrok authtoken
