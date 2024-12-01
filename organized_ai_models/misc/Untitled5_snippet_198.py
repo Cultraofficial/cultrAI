3ffa6336784a6d2e86c141178ab9c90b0b044a43
Cultraofficial <tropicalstormllc@gmail.com>
2024-12-01 09:09:31 -0500

    Add organized_ai_models/misc/Untitled5_snippet_198.py

---
+++ organized_ai_models/misc/Untitled5_snippet_198.py
@@ -0,0 +1,8 @@
+# Upload processed notebooks back to Google Cloud Storage
+for filename in os.listdir(local_directory):
+    local_path = os.path.join(local_directory, filename)
+    remote_path = folder_path + "final/" + filename  # Save in a 'final' subfolder in the processed directory
+
+    blob = bucket.blob(remote_path)
+    blob.upload_from_filename(local_path)
+    print(f"Uploaded {filename} to {remote_path}")
